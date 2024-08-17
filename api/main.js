963a01751e01799145c8f61167599d3530c90bd2
Lins T <abrahamkyere270@gmail.com>
2024-08-17 02:28:21 +0000

    vanila

--- api/main.js
+++ api/main.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
  export default function handler(req, res) {
- res.status(200).json({ "moment": moment})
+ res.status(200).json({ message: moment()})
  //Done
  //res.status(200).json({ moment: moment })
 } 
