fb6f7943b17e5d6ee69a2a3af418825ac1b0074d
Lins T <abrahamkyere270@gmail.com>
2024-08-18 00:52:15 +0000

    vanila

--- public/js/modules/calender_module1.js
+++ public/js/modules/calender_module1.js
@@ -6,7 +6,7 @@ import clicks from '../main.js'
 async function _jsonFtch() {
  let response1 = await fetch('../api/main.js')
  let data1 = await response1.json()
- let settter = await data1.good1
+ let setter = await data1.good1
  await console.log(setter.year())
  let now = await data1.good2
  await console.log(now('dd DDD MMM YYYY'))
