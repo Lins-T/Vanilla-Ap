2b3ac95ffa82dd5d34fe4cbec65c127754fe13b4
Lins T <abrahamkyere270@gmail.com>
2024-08-18 00:57:18 +0000

    vanila

--- api/main.js
+++ api/main.js
@@ -2,5 +2,5 @@ import moment from 'moment'
 
  export default function handler(req, res) {
  const now = moment()
- res.status(200).json({ good1: now, good2: moment()})
+ res.status(200).json({ good1: moment, good2: moment()})
 } 
