95e9c67477c54904252720b44c82647f479f0bbf
Lins T <abrahamkyere270@gmail.com>
2024-08-19 02:29:00 +0000

    vanila

--- api/main.js
+++ api/main.js
@@ -6,13 +6,14 @@ console.log('Hello fxn')
 }
 
 const obj = {
- fxn() {
+name: "Yaw",
+fxn() {
  return moment 
  }
 }
 let string = JSON.stringify(obj)
 let objSon = JSON.parse(string)
  export default function handler(req, res) {
- res.status(200).json({ detail: objSon,message: `${hello}`, info: "Hello world"})
+ res.status(200).json({ detail: objSon,message: `${hello}`, info: "Infinite world"})
 }
 
